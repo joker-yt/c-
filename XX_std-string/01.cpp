13b3e5d5b5bcde2b5931ac49ed0c0a68a0fa41b6
yasu <jokerxx1344xxyt@gmail.com>
2017-07-04 09:11:49 +0900

    std::string

---
+++ XX_std-string/01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Enter the message:" << std::endl;
+
+  std::string str;
+  std::cin >> str;
+
+  std::cout << "your message is : " << str << std::endl;
+  return 0;
+}
